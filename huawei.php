e9458eb64ed9c7ea8856265cd32e889c5104c714
Liseth Delgado <cdelgadolis@gmail.com>
2019-02-13 00:22:58 -0400

    subiendo cambios

---
+++ huawei.php
@@ -0,0 +1,29 @@
+<div class="fondo-azul">
+  <table border="0">
+    <tr>
+      <td width="600px">
+        <img src="LandingZonalaptop/Links/banner-s1-2.png" width="450px" height="450px">
+      </td>
+      <td>
+        <p class="letras-blancas">
+          Celular Huawei P20 <br>Lite Octa Core 4GB<br>Triple Cam
+          <br>
+          <span class="letras-blancas-1">
+            14 coutas de<br>
+          </span>
+            <img src="img/$756.png"><br>
+            <span class="letras-blancas-1">
+            Precio original: $12.913 IVA inc.<br>
+            P.T.F: $10.589 IVA inc.
+          </span><br><br>
+          <span class="letras-blancas-2">
+          Pedí el tuyo ahora: <br><br>
+          <button type="button" class="btn btn-outline-primary"><i class="fab fa-whatsapp"></i></button>
+          <button type="button" class="btn btn-outline-primary"><i class="fab fa-instagram"></i></button>
+          <button type="button" class="btn btn-outline-primary"><i class="fas fa-map-marker-alt"></i></button>
+          </span>
+        </p>
+      </td>
+    </tr>
+  </table>
+</div>
