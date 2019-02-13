3cca68e28368cb8913367ca64b27c63816876836
Liseth Delgado <cdelgadolis@gmail.com>
2019-02-13 00:23:51 -0400

    imagenes editadas

---
+++ parlante.php
@@ -0,0 +1,29 @@
+<div class="fondo-azul">
+  <table border="0">
+    <tr>
+      <td width="600px">
+        <img src="LandingZonalaptop/Links/banner-parlante2.png" width="400px" height="450px" align="right">
+      </td>
+      <td>
+        <p class="letras-blancas">
+          Parlante Portátil <br>Bluetooth Hi-Fi<br>North Tech W45
+          <br>
+          <span class="letras-blancas-1">
+            14 coutas de<br>
+          </span>
+            <img src="img/$128.png"><br>
+            <span class="letras-blancas-1">
+            Precio original: $2.183 IVA inc.<br>
+            P.T.F: $1.790 IVA inc.
+          </span><br><br>
+          <span class="letras-blancas-2">
+          Pedí el tuyo ahora: <br><br>
+          <button type="button" class="btn btn-outline-primary"><i class="fab fa-whatsapp"></i></button>
+          <button type="button" class="btn btn-outline-primary"><i class="fab fa-instagram"></i></button>
+          <button type="button" class="btn btn-outline-primary"><i class="fas fa-map-marker-alt"></i></button>
+          </span>
+        </p>
+      </td>
+    </tr>
+  </table>
+</div>
---
+++ play-station.php
@@ -0,0 +1,28 @@
+<div class="fondo-azul">
+  <table border="0">
+    <tr>
+      <td width="600px">
+        <img src="LandingZonalaptop/Links/playstation-4-slim-cnet copia.png" width="500px" height="450px">
+      </td>
+      <td>
+        <p class="letras-blancas">
+          Play Station Ps4<br>Slim 1tb Fifa 2019<br>
+          <span class="letras-blancas-1">
+            14 coutas de<br>
+          </span>
+            <img src="img/$1501.png"><br>
+            <span class="letras-blancas-1">
+            Precio original: $24.013 IVA inc.<br>
+            P.T.F: $21.013 IVA inc.
+          </span><br><br>
+          <span class="letras-blancas-2">
+          Pedí el tuyo ahora: <br><br>
+          <button type="button" class="btn btn-outline-primary"><i class="fab fa-whatsapp"></i></button>
+          <button type="button" class="btn btn-outline-primary"><i class="fab fa-instagram"></i></button>
+          <button type="button" class="btn btn-outline-primary"><i class="fas fa-map-marker-alt"></i></button>
+          </span>
+        </p>
+      </td>
+    </tr>
+  </table>
+</div>
